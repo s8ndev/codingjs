16f4396b54e51f480b09f4bb799c602396ddc455
s8ndev <s8ndev@gmail.com>
2023-01-30 19:35:04 +0500

    Add makes10 answer

---
+++ Warmup-1/makes10.js
@@ -0,0 +1,18 @@
+/*
+Warmup-1 -- makes10
+Given 2 ints, a and b, return true if one if them is 10 or if their sum is 10.
+
+Examples
+
+makes10(9, 10) → true
+makes10(9, 9) → false
+makes10(1, 9) → true
+*/
+
+function makes10(a, b) {
+  return a + b === 10 || a === 10 || b === 10;
+}
+
+console.log(makes10(9, 10));
+console.log(makes10(9, 9));
+console.log(makes10(1, 9));
