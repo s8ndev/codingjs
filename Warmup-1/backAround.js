c5d3b33aa88c36e3e4fc3bcda1b820e089e70e3e
s8ndev <s8ndev@gmail.com>
2023-01-30 22:29:49 +0500

    Add backAround answer

--- Warmup-1/backAround.js
+++ Warmup-1/backAround.js
@@ -1,6 +1,7 @@
 /*
 Warmup-1 -- backAround
-Given a string, take the last char and return a new string with the last char added at the front and back, so 'cat' yields 'tcatt' The original string will be length 1 or more.
+Given a string, take the last char and return a new string with the last char added at the front and back,
+so 'cat' yields 'tcatt' The original string will be length 1 or more.
 
 Examples
 
