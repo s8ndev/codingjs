c40233bb65ea337c40075d6d19a4e8fa57854f5b
s8ndev <s8ndev@gmail.com>
2023-01-30 18:58:02 +0500

    Add diff21 answer

---
+++ Warmup-1/diff21.js
@@ -0,0 +1,18 @@
+/*
+Warmup-1 -- diff21
+Given an int n, return the absolute difference between n and 21, except return double the absolute difference if n is over 21.
+
+Examples
+
+diff21(19) → 2
+diff21(10) → 11
+diff21(21) → 0
+*/
+
+function diff21(n) {
+  return n > 21 ? Math.abs((n - 21) * 2) : 21 - n;
+}
+
+console.log(diff21(19));
+console.log(diff21(10));
+console.log(diff21(21));
