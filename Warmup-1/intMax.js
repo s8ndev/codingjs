a5fb5ecb394b32b6ec2631a411cdcb16d4c8e9d0
s8ndev <s8ndev@gmail.com>
2023-01-31 22:02:13 +0500

    Add intMax answer

---
+++ Warmup-1/intMax.js
@@ -0,0 +1,18 @@
+/*
+Warmup-1 -- intMax
+Given three int values, a b c, return the largest.
+
+Examples
+
+intMax(1, 2, 3) → 3
+intMax(1, 3, 2) → 3
+intMax(3, 2, 1) → 3
+*/
+
+function intMax(a, b, c) {
+  return Math.max(a, b, c);
+}
+
+console.log(intMax(1, 2, 3));
+console.log(intMax(1, 3, 2));
+console.log(intMax(3, 2, 1));
