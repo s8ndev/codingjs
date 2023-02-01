06445a6c05d29e597863bfb74302d0a8744f2206
s8ndev <s8ndev@gmail.com>
2023-02-01 20:32:21 +0500

    Add everyNth answer

---
+++ Warmup-1/everyNth.js
@@ -0,0 +1,21 @@
+/*
+Warmup-1 -- everyNth
+Given a non-empty string and an int N, return the string made starting with char 0,
+and then every Nth char of the string. So if N is 3, use char 0, 3, 6, ... and so on. N is 1 or more.
+
+Examples
+
+everyNth('Miracle', 2) → Mrce
+everyNth('abcdefg', 2) → aceg
+everyNth('abcdefg', 3) → adg
+*/
+
+function everyNth(str, n) {
+  let newStr = "";
+  for (let i = 0; i < str.length; i += n) newStr += str[i];
+  return newStr;
+}
+
+console.log(everyNth("Miracle", 2));
+console.log(everyNth("abcdefg", 2));
+console.log(everyNth("abcdefg", 3));
