71ffa416e6f76ec49551d9a9d42b00e17e70ec1f
s8ndev <s8ndev@gmail.com>
2023-02-01 20:17:19 +0500

    Add endUp answer

---
+++ Warmup-1/endUp.js
@@ -0,0 +1,22 @@
+/*
+Warmup-1 -- endUp
+Given a string, return a new string where the last 3 chars are now in upper case.
+If the string has less than 3 chars, uppercase whatever is there.
+Note that str.toUpperCase() returns the uppercase version of a string.
+
+Examples
+
+endUp('Hello') → HeLLO
+endUp('hi there') → hi thERE
+endUp('hi') → HI
+*/
+
+function endUp(str) {
+  return str.length > 2
+    ? str.slice(0, str.length - 3) + str.slice(str.length - 3, str.length).toUpperCase()
+    : str.toUpperCase();
+}
+
+console.log(endUp("Hello"));
+console.log(endUp("hi there"));
+console.log(endUp("hi"));
