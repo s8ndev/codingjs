d5163452a2bc4f95956f65bbed03986bad1043c0
s8ndev <s8ndev@gmail.com>
2023-02-02 17:07:51 +0500

    Add stringTimes answer

---
+++ Warmup-2/stringTimes.js
@@ -0,0 +1,18 @@
+/*
+Warmup-2 -- stringTimes
+Given a string and a non-negative int n, return a larger string that is n copies of the original string.
+
+Examples
+
+stringTimes('Hi', 2) → HiHi
+stringTimes('Hi', 3) → HiHiHi
+stringTimes('Hi', 1) → Hi
+*/
+
+function stringTimes(str, n) {
+  return str.repeat(n);
+}
+
+console.log(stringTimes("Hi", 2));
+console.log(stringTimes("Hi", 3));
+console.log(stringTimes("Hi", 1));
