12089aa963eb96b72802952517796df86566fdb3
s8ndev <s8ndev@gmail.com>
2023-02-05 21:50:18 +0500

    Add helloName answer

---
+++ String-1/helloName.js
@@ -0,0 +1,18 @@
+/*
+String-1 -- AliceAlice
+Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+
+Examples
+
+helloName('Bob') → Hello Bob!
+helloName('Alice') → Hello Alice!
+helloName('X') → Hello X!
+*/
+
+function helloName(name) {
+  return `Hello ${name}!`;
+}
+
+console.log(helloName("Bob"));
+console.log(helloName("Alice"));
+console.log(helloName("X"));
