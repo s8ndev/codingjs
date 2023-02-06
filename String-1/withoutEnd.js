76c5abc37ca590a9e3d8137194015a35007cf9b7
s8ndev <s8ndev@gmail.com>
2023-02-06 20:16:11 +0500

    Add withoutEnd answer

---
+++ String-1/withoutEnd.js
@@ -0,0 +1,19 @@
+/*
+String-1 -- withoutEnd
+Given a string, return a version without the first and last char, so "Hello" yields "ell".
+The string length will be at least 2.
+
+Examples
+
+withoutEnd('Hello') → ell
+withoutEnd('java') → av
+withoutEnd('coding') → odin
+*/
+
+function withoutEnd(str) {
+  return str.slice(1, str.length - 1);
+}
+
+console.log(withoutEnd("Hello"));
+console.log(withoutEnd("java"));
+console.log(withoutEnd("coding"));
