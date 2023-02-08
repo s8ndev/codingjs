1fcff714df8b99553dedefc472fbc112540abd0f
s8ndev <s8ndev@gmail.com>
2023-02-08 17:27:08 +0500

    Add xyBalance answer

---
+++ String-2/xyBalance.js
@@ -0,0 +1,20 @@
+/*
+String-2 -- xyBalance
+We'll say that a String is xy-balanced if for all the 'x' chars in the string,
+there exists a 'y' char somewhere later in the string. So "xxy" is balanced,
+but "xyx" is not. One 'y' can balance multiple 'x's. Return true if the given string is xy-balanced.
+
+Examples
+
+xyBalance('aaxbby') → true
+xyBalance('aaxbb') → false
+xyBalance('yaaxbb') → false
+*/
+
+function xyBalance(str) {
+  return str.lastIndexOf("x") <= str.lastIndexOf("y");
+}
+
+console.log(xyBalance("aaxbby"));
+console.log(xyBalance("aaxbb"));
+console.log(xyBalance("yaaxbb"));
