fabeac01832f8fdea0df10e1b454828a5e08f252
s8ndev <s8ndev@gmail.com>
2023-02-09 22:06:25 +0500

    Add gHappy answer

---
+++ String-3/gHappy.js
@@ -0,0 +1,26 @@
+/*
+String-3 -- gHappy
+We'll say that a lowercase 'g' in a string is "happy" if there is another 'g'
+immediately to its left or right. Return true if all the g's in the given string are happy.
+
+Examples
+
+gHappy('xxggxx') → true
+gHappy('xxgxx') → false
+gHappy('xxggyygxx') → false
+*/
+
+function gHappy(str) {
+  let happy = true;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === "g") {
+      if (
+        (i > 0 && str[i - 1] === "g") ||
+        (i < str.length - 1 && str[i + 1] === "g")
+      )
+        happy = true;
+      else happy = false;
+    }
+  }
+  return happy;
+}
