6e65f4f9c91f8172e042ec04c89dd3ca04083809
s8ndev <s8ndev@gmail.com>
2023-02-13 22:57:46 +0500

    Add love6 answer

---
+++ Logic-1/love6.js
@@ -0,0 +1,19 @@
+/*
+Logic-1 -- love6
+The number 6 is a truly great number. Given two int values, a and b, return true if either one is 6.
+Or if their sum or difference is 6. Note: the function Math.abs(num) computes the absolute value of a number.
+
+Examples
+
+love6(6, 4) → true
+love6(4, 5) → false
+love6(1, 5) → true
+*/
+
+function love6(a, b) {
+  return a === 6 || b === 6 || a + b === 6 || Math.abs(a - b) === 6;
+}
+
+console.log(love6(6, 4));
+console.log(love6(4, 5));
+console.log(love6(1, 5));
